17f3dbb48c845639c08a06b0a35c0b3c435e061f
Jason Green <jason@jasg.org>
2016-09-25 00:55:20 +0000

    update year in license

--- stenomod.ino
+++ stenomod.ino
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2009-2015 by Jason Green.
+Copyright (C) 2016 by Jason Green.
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
